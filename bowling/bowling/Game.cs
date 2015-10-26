e4e3d5cac497cf0c21b63b31792fbcdfea61d7c2
aska <aska@ntnu.edu.tw>
2015-10-26 09:47:43 +0800

    fir version for bowling game kata

---
+++ bowling/bowling/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bowling
+{
+    class Game
+    {
+        internal void SetPins(int p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int GetFinalScore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ bowling/bowling/TestBowling.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bowling
+{
+    [TestFixture]
+    public class TestBowling
+    {
+        [Test]
+        public void AllZero()
+        {
+            Game g = new Game();
+            for (int i = 0; i < 20; i++)
+            {
+                g.SetPins(0);
+            }
+            Assert.That(g.GetFinalScore(), Is.EqualTo(0));
+        }
+    }
+}
